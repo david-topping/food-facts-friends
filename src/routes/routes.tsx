ca3604eaab805b715506647dba6f58d4e460f065
David Topping <topping.david11@gmail.com>
2025-12-24 18:27:53 +0000

    feat: adds what we offer

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -15,27 +15,27 @@ export const APP_ROUTES: AppRoute[] = [
     showInNav: true,
   },
   {
-    path: "/about",
-    label: "About",
-    element: <div>About</div>,
+    path: "/services",
+    label: "What We Offer",
+    element: <div>Services</div>,
     showInNav: true,
   },
   {
     path: "/donate",
-    label: "Donate",
+    label: "How to Donate",
     element: <div>Donate</div>,
-    showInNav: true,
-  },
-  {
-    path: "/contact",
-    label: "Contact",
-    element: <div>Contact</div>,
     showInNav: true,
   },
   {
     path: "/our-supporters",
     label: "Our Supporters",
     element: <div>Our Supporters</div>,
+    showInNav: true,
+  },
+  {
+    path: "/contact",
+    label: "Contact Us",
+    element: <div>Contact</div>,
     showInNav: true,
   },
 ];
